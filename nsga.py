9614a71ba15250fa6af9044af945dcc7f2779613
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-07-28 00:59:47 -0300

    Running nsga-ii version 1.0

--- nsga.py
+++ nsga.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
 import numpy as np
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.factory import get_crossover, get_mutation, get_termination
+from pymoo.operators.crossover.pntx import TwoPointCrossover
+from pymoo.operators.mutation.pm import PolynomialMutation
 from pymoo.optimize import minimize
+from pymoo.visualization.scatter import Scatter
+import gutils
 
 n_v = 12
 snr_min, tp_min, sf_min = -5.5, 2.0, 7.0
@@ -18,8 +22,9 @@ xu_sf = [sf_max for _ in range(n_v)]
 xu = np.concatenate((xu_snr, xu_tp, xu_sf))
 
 min_d = 1
-
-pop_size = 100
+pop_size = 80
+n_var = 36
+n_obj = 2
 
 def generate_vertexes(min_val, max_val, min_dist):
   coords = np.zeros(12)
@@ -40,35 +45,35 @@ def generate_vertexes(min_val, max_val, min_dist):
     coords[3:6].sort()
     flag = False
   
-  if coords[5] <= coords[2]:
-    coords[5] = coords[2] + np.random.uniform(0.05, 0.25)
-    coords[5] = min(max_val, coords[5])
-  
   flag = True
   while flag or coords[8] - coords[6] < min_dist:
-    coords[6] = np.random.uniform(coords[0], coords[5])
+    coords[6] = np.random.uniform(coords[3], coords[5])
     coords[7:9] = np.random.uniform(coords[6], max_val, 2)
     coords[6:9].sort()
     flag = False
-
-  if coords[3] >= coords[6]:
-    coords[3] = coords[6] - np.random.uniform(0.05, 0.25)
-    coords[3] = max(min_val, coords[3])
-
+  
   flag = True
   while flag or coords[11] - coords[9] < min_dist:
-    coords[9] = np.random.uniform(coords[0], coords[8])
+    coords[9] = np.random.uniform(coords[6], coords[8])
     coords[10] = np.random.uniform(coords[9], max_val)
     coords[9:12].sort()
     flag = False
   
+  """if coords[5] <= coords[2]:
+    coords[5] = coords[6] + np.random.uniform(0.05, 0.25)
+    coords[5] = min(max_val, coords[5])  
+
+  if coords[3] >= coords[6] and coords[5] <= coords[8]:
+    coords[3] = coords[2] - np.random.uniform(0.05, 0.25)
+    coords[3] = max(min_val, coords[3])
+  
   if coords[8] <= coords[5]:
-    coords[8] = coords[5] + np.random.uniform(0.05, 0.25)
+    coords[8] = coords[9] + np.random.uniform(0.05, 0.25)
     coords[8] = min(max_val, coords[8])
 
   if coords[6] >= coords[9]:
-    coords[6] = coords[9] - np.random.uniform(0.05, 0.25)
-    coords[6] = max(min_val, coords[6])
+    coords[6] = coords[5] - np.random.uniform(0.05, 0.25)
+    coords[6] = max(min_val, coords[6])"""
 
   return coords
 
@@ -93,57 +98,53 @@ def check_v(v, min_val, max_val, min_dist):
     v[5] += np.random.uniform(0, 0.25)
     v[5] = min(max_val, v[5])
     v[3:6].sort()
-  
-  if v[3] > v[2]:
-    v[3], v[2] = swap(v[3], v[2])
-  
-  if v[5] <= v[2]:
-    v[5] = v[2] + np.random.uniform(0.05, 0.25)
-    v[5] =  min(max_val, v[5])
 
   v[6:9].sort()
   while v[8] - v[6] < min_dist:
     v[8] += np.random.uniform(0, 0.25)
     v[8] = min(max_val, v[8])
     v[6:9].sort()
-  
-  if v[5] < v[6]:
-    v[5], v[6] = swap(v[5], v[6])
-  
-  if v[3] >= v[6]:
-    v[3] = v[6] - np.random.uniform(0.05, 0.25)
-    v[3] =  max(min_val, v[3])
-  
-  if v[8] <= v[5]:
-    v[8] = v[5] + np.random.uniform(0.05, 0.25)
-    v[8] = min(max_val, v[8])
-  
+
   v[9:12].sort()
   while v[11] - v[9] < min_dist:
     v[9] -= np.random.uniform(0, 0.25)
     v[9] = max(min_val, v[9])
     v[9:12].sort()
   
+  if v[3] > v[2]:
+    v[3], v[2] = swap(v[3], v[2])
+  
+  if v[6] > v[5]:
+    v[6], v[5] = swap(v[6], v[5])
+  
+  if v[9] > v[8]:
+    v[9], v[8] = swap(v[9], v[8])
+
+  """if v[5] <= v[2]:
+    v[5] = v[6] + np.random.uniform(0.05, 0.25)
+    v[5] =  min(max_val, v[5])
+  
+  if v[3] >= v[6] and v[5] <= 8:
+    v[3] = v[2] - np.random.uniform(0.05, 0.25)
+    v[3] =  max(min_val, v[3])
+  
+  if v[8] <= v[5]:
+    v[8] = v[9] + np.random.uniform(0.05, 0.25)
+    v[8] = min(max_val, v[8])
+  
   if v[6] >= v[9]:
-    v[6] = v[9] - np.random.uniform(0.05, 0.25)
-    v[6] = max(min_val, v[6])
+    v[6] = v[5] - np.random.uniform(0.05, 0.25)
+    v[6] = max(min_val, v[6])"""
 
 class FuzzyProblem(Problem):
   def __init__(self):
-    X = []
-    for i in range(pop_size):
-      snr = generate_vertexes(snr_min, snr_max, min_d)
-      tp = generate_vertexes(tp_min, tp_max, min_d)
-      sf = generate_vertexes(sf_min, sf_max, min_d)
-      X.append(np.concatenate((snr, tp, sf)))
-
-    super().__init__(n_var=36,
-                      n_obj=2,
+    self.num_run = 0
+    super().__init__(n_var=n_var,
+                      n_obj=n_obj,
                       n_constr=0,
                       xl=xl,
                       xu=xu,
-                      elementwise_evaluation=True,
-                      X=X)
+                      elementwise_evaluation=True)
 
   def _check_x(self, x):
     snr = x[0:12]
@@ -154,30 +155,57 @@ class FuzzyProblem(Problem):
     check_v(tp, tp_min, tp_max, min_d)
     check_v(sf, sf_min, sf_max, min_d)
 
+  def _simulate(self, x):
+    results = []
+    for i in x:      
+      gutils.fill_fll2(i)
+      gutils.simulate_range()
+      pdr, energy = gutils.calc_range()
+      results.append(np.array([-pdr, energy]))
+
+    return np.array(results)
+
   def _evaluate(self, x, out, *args, **kwargs):
-    self._check_x(x)
-    f1 = np.random.uniform(0.1, 5.0)
-    f2 = -np.random.uniform(1, 136)
-    out["F"] = [f1, f2]
+    if self.num_run > 0:
+      for i in x:
+        self._check_x(i)
+    
+    out['F'] = [self._simulate(x)]
+    
+    self.num_run += 1
 
 def execute():
-  problem = FuzzyProblem()
-  algorithm = NSGA2(pop_size=100, 
-                    mutation=get_mutation('real_pm', prob=0.2),
-                    crossover=get_crossover('real_two_point', prob=0.95),
-                    eliminate_duplicates=True)
+  X = []
+  for i in range(pop_size):
+    snr = generate_vertexes(snr_min, snr_max, min_d)
+    tp = generate_vertexes(tp_min, tp_max, min_d)
+    sf = generate_vertexes(sf_min, sf_max, min_d)
+    X.append(np.concatenate((snr, tp, sf)))
 
-  termination = get_termination('n_gen', 100)
+  problem = FuzzyProblem()
+  algorithm = NSGA2(pop_size=pop_size, 
+                    mutation=PolynomialMutation(prob=0.2),
+                    crossover=TwoPointCrossover(prob=1.0),
+                    eliminate_duplicates=True,
+                    sampling=np.array(X))
+
   res = minimize(problem,
                 algorithm,
-                termination,
-                seed=1,
+                ('n_gen', 90),
+                save_history=True,
                 verbose=True)
 
-  print("Soluções encontradas:")
-  for solution in res.X:
+  print('Solutions founded:')
+  for solution in res.F:
     print(solution)
 
+  X = res.X
+  print('X', X)
+
+  plot = Scatter()
+  plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+  plot.add(res.F, facecolor="none", edgecolor="red")
+  plot.show()
+
 if __name__ == '__main__':
-  pass
-  
+  execute()
